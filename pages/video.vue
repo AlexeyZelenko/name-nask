5480437b75f45056e579655135dcd0150f91f487
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-12-30 19:06:46 +0200

    Start

--- pages/video.vue
+++ pages/video.vue
@@ -5,7 +5,7 @@
       <v-container>
         <v-row>
           <v-col
-            v-for="n in 6"
+            v-for="n in 3"
             :key="n"
             cols="12"
             md="4"
@@ -18,23 +18,36 @@
                   class="mx-auto"
                   max-width="400"
                 >
-                  <v-img
-                    class="white--text align-end"
-                    height="200px"
-                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                  >
-                    <v-card-title>Top 10 Australian beaches</v-card-title>
-                  </v-img>
+<!--                  <v-img-->
+<!--                    class="white&#45;&#45;text align-end"-->
+<!--                    height="200px"-->
+<!--                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"-->
+<!--                  >-->
+<!--                    <v-card-title>Top 10 Australian beaches</v-card-title>-->
+<!--                  </v-img>-->
+                  <!-- youtube iframe with progressive enhancement (extra queries after the url to optimize the embed) -->
+                  <vue-plyr>
+                    <div class="plyr__video-embed">
+<!--                      <v-card-title>Top 10 Australian beaches</v-card-title>-->
+                      <iframe
+                        src="https://www.youtube.com/embed/A5MrLlrDqwE?amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+                        allowfullscreen
+                        allowtransparency
+                        allow="autoplay"
+                      ></iframe>
+                    </div>
+                  </vue-plyr>
 
+                  <v-card-title text-center>NASK - Holy Night</v-card-title>
                   <v-card-subtitle class="pb-0">
-                    Number 10
+                    NASK
                   </v-card-subtitle>
 
-                  <v-card-text class="text--primary">
-                    <div>Whitehaven Beach</div>
+<!--                  <v-card-text class="text&#45;&#45;primary">-->
+<!--                    <div>Whitehaven Beach</div>-->
 
-                    <div>Whitsunday Island, Whitsunday Islands</div>
-                  </v-card-text>
+<!--                    <div>Whitsunday Island, Whitsunday Islands</div>-->
+<!--                  </v-card-text>-->
 
                   <v-card-actions>
                     <v-btn
@@ -59,22 +72,11 @@
       </v-container>
     </v-item-group>
   </template>
-  <!-- youtube iframe with progressive enhancement (extra queries after the url to optimize the embed) -->
-  <vue-plyr>
-    <div class="plyr__video-embed">
-      <iframe
-        src="https://www.youtube.com/embed/bTqVqk7FSmY?amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
-        allowfullscreen
-        allowtransparency
-        allow="autoplay"
-      ></iframe>
-    </div>
-  </vue-plyr>
 
   <!-- youtube div element -->
-  <vue-plyr>
-    <div data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
-  </vue-plyr>
+<!--  <vue-plyr>-->
+<!--    <div data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>-->
+<!--  </vue-plyr>-->
 </div>
 </template>
 
