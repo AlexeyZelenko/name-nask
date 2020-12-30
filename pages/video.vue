24350130303be4d53bc5d3b3ffddcb13084a1c08
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-12-30 18:54:02 +0200

    Start

--- pages/video.vue
+++ pages/video.vue
@@ -1,5 +1,64 @@
 <template>
 <div>
+  <template>
+    <v-item-group>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="n in 6"
+            :key="n"
+            cols="12"
+            md="4"
+          >
+            <v-item>
+              <div
+                class="display-3 flex-grow-1 text-center"
+              >
+                <v-card
+                  class="mx-auto"
+                  max-width="400"
+                >
+                  <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                  >
+                    <v-card-title>Top 10 Australian beaches</v-card-title>
+                  </v-img>
+
+                  <v-card-subtitle class="pb-0">
+                    Number 10
+                  </v-card-subtitle>
+
+                  <v-card-text class="text--primary">
+                    <div>Whitehaven Beach</div>
+
+                    <div>Whitsunday Island, Whitsunday Islands</div>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-btn
+                      color="orange"
+                      text
+                    >
+                      Share
+                    </v-btn>
+
+                    <v-btn
+                      color="orange"
+                      text
+                    >
+                      Explore
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </div>
+            </v-item>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-item-group>
+  </template>
   <!-- youtube iframe with progressive enhancement (extra queries after the url to optimize the embed) -->
   <vue-plyr>
     <div class="plyr__video-embed">
