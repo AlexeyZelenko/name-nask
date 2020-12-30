05b7811af70572a03ff3f22560dd3388aac7337c
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-12-30 13:19:45 +0200

    Initial commit

---
+++ pages/inspire.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- audio element -->
+    <vue-plyr>
+      <audio controls crossorigin playsinline>
+        <source
+          src='/HolyNight.mp3'
+          type='audio/mp3'
+        />
+        <source
+          src="/path/to/audio.ogg"
+          type="audio/ogg"
+        />
+      </audio>
+    </vue-plyr>
+
+  </div>
+</template>
